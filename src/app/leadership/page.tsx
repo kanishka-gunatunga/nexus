e740015ba14ed198e54d8a6cfe884e69e9690356
ksulakshana02 <sulakshanakaveesha@gmail.com>
2025-07-11 14:16:44 +0530

    client changes

--- src/app/leadership/page.tsx
+++ src/app/leadership/page.tsx
@@ -165,7 +165,7 @@ const LeadershipPage = () => {
                         {/*    ))}*/}
                         {/*</div>*/}
 
-                        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6 sm:gap-8 lg:gap-12">
+                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 lg:gap-12">
                             {teamMembers.map((member) => (
                                 <div
                                     key={member.id}
