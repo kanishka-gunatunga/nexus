20f68084c067dc92f8485d865db7fe2656711ef9
Dilki687 <chamodidilki44@gmail.com>
2025-08-01 12:32:51 +0530

    link services in home mobile layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -759,87 +759,89 @@ export default function Home() {
             <AnimatedSection direction="up" delay={0.2}>
               <Slider {...settings}>
                 {services.map((item, index) => (
-                  <div
-                    key={index}
-                    className="px-2 py-4 flex flex-col items-center"
-                  >
+                  <Link key={index} href={item.link}>
                     <div
-                      className={`relative group cursor-pointer rounded-lg overflow-hidden transition-all duration-300
+                      key={index}
+                      className="px-2 py-4 flex flex-col items-center"
+                    >
+                      <div
+                        className={`relative group cursor-pointer rounded-lg overflow-hidden transition-all duration-300
                                             ${
                                               activeService.label === item.label
                                                 ? "scale-[1.05] bg-[#002453] shadow-lg"
                                                 : "scale-[0.9]"
                                             }`}
-                      style={{
-                        height:
-                          activeService.label === item.label
-                            ? "280px"
-                            : "270px",
-                        width: "100%",
-                      }}
-                    >
-                      {activeService.label !== item.label && (
-                        <Image
-                          src={item.src}
-                          alt={item.alt}
-                          fill
-                          className="object-cover transition-transform duration-300 group-hover:scale-105"
-                        />
-                      )}
+                        style={{
+                          height:
+                            activeService.label === item.label
+                              ? "280px"
+                              : "270px",
+                          width: "100%",
+                        }}
+                      >
+                        {activeService.label !== item.label && (
+                          <Image
+                            src={item.src}
+                            alt={item.alt}
+                            fill
+                            className="object-cover transition-transform duration-300 group-hover:scale-105"
+                          />
+                        )}
 
-                      <div
-                        className={`absolute inset-0 transition-all duration-300
+                        <div
+                          className={`absolute inset-0 transition-all duration-300
                                             ${
                                               activeService.label === item.label
                                                 ? "bg-[#002B64]/10"
                                                 : "bg-gradient-to-t from-[#002B64]/40 to-[#00255700]/10"
                                             }`}
-                      />
+                        />
 
-                      {activeService.label === item.label ? (
-                        <>
-                          <div className="absolute bg-[url('/x.png')] opacity-5 bg-no-repeat bg-contain inset-0 items-center -top-20 w-[400px] h-[800px]" />
-                          <div className="relative p-2 sm:p-4 flex flex-col items-center justify-between h-full">
-                            <p className="text-left font-medium pt-2 px-6 text-xs text-[#FFFFFF] leading-[20px] whitespace-pre-line">
-                              {item.description}
-                            </p>
-                            <div className="flex flex-row gap-2 group justify-start items-center pt-4 sm:pt-6 w-full px-6">
-                              <svg
-                                className="w-8 h-8 text-left flex-shrink-0"
-                                width="81"
-                                height="82"
-                                viewBox="0 0 81 82"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                              >
-                                <circle
-                                  cx="40.5"
-                                  cy="41"
-                                  r="37.7635"
-                                  stroke="white"
-                                  strokeWidth="5.47297"
-                                />
-                                <path
-                                  d="M50.7383 33.4925C50.7693 32.847 50.5433 32.2156 50.1099 31.7363C49.6764 31.257 49.0708 30.9689 48.4254 30.935L34.6517 30.2429C34.3319 30.2269 34.0121 30.274 33.7105 30.3815C33.4089 30.489 33.1314 30.6549 32.894 30.8697C32.4143 31.3034 32.1266 31.9099 32.0942 32.5558C32.0617 33.2016 32.2872 33.8339 32.7209 34.3136C33.1547 34.7932 33.7612 35.0809 34.4070 35.1133L42.3021 35.51L29.3090 47.26C28.8294 47.6937 28.5418 48.3001 28.5093 48.9459C28.4769 49.5916 28.7023 50.2238 29.1360 50.7034C29.5697 51.183 30.1761 51.4706 30.8219 51.5031C31.4676 51.5355 32.0998 51.3101 32.5794 50.8764L45.5725 39.1264L45.1759 47.0215C45.1598 47.3413 45.2069 47.6612 45.3144 47.9628C45.4220 48.2644 45.5878 48.5418 45.8026 48.7793C46.0174 49.0168 46.2768 49.2097 46.5661 49.3469C46.8554 49.4841 47.1689 49.5630 47.4887 49.5791C47.8085 49.5952 48.1283 49.5481 48.4299 49.4405C48.7315 49.3330 49.0090 49.1671 49.2465 48.9523C49.4840 48.7376 49.6768 48.4781 49.8141 48.1888C49.9513 47.8995 50.0302 47.5860 50.0463 47.2662L50.7383 33.4925Z"
-                                  fill="white"
-                                />
-                              </svg>
-                              <h3 className="text-sm sm:text-base text-left leading-[20px] text-white font-medium">
-                                {item.label}
-                              </h3>
+                        {activeService.label === item.label ? (
+                          <>
+                            <div className="absolute bg-[url('/x.png')] opacity-5 bg-no-repeat bg-contain inset-0 items-center -top-20 w-[400px] h-[800px]" />
+                            <div className="relative p-2 sm:p-4 flex flex-col items-center justify-between h-full">
+                              <p className="text-left font-medium pt-2 px-6 text-xs text-[#FFFFFF] leading-[20px] whitespace-pre-line">
+                                {item.description}
+                              </p>
+                              <div className="flex flex-row gap-2 group justify-start items-center pt-4 sm:pt-6 w-full px-6">
+                                <svg
+                                  className="w-8 h-8 text-left flex-shrink-0"
+                                  width="81"
+                                  height="82"
+                                  viewBox="0 0 81 82"
+                                  fill="none"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                >
+                                  <circle
+                                    cx="40.5"
+                                    cy="41"
+                                    r="37.7635"
+                                    stroke="white"
+                                    strokeWidth="5.47297"
+                                  />
+                                  <path
+                                    d="M50.7383 33.4925C50.7693 32.847 50.5433 32.2156 50.1099 31.7363C49.6764 31.257 49.0708 30.9689 48.4254 30.935L34.6517 30.2429C34.3319 30.2269 34.0121 30.274 33.7105 30.3815C33.4089 30.489 33.1314 30.6549 32.894 30.8697C32.4143 31.3034 32.1266 31.9099 32.0942 32.5558C32.0617 33.2016 32.2872 33.8339 32.7209 34.3136C33.1547 34.7932 33.7612 35.0809 34.4070 35.1133L42.3021 35.51L29.3090 47.26C28.8294 47.6937 28.5418 48.3001 28.5093 48.9459C28.4769 49.5916 28.7023 50.2238 29.1360 50.7034C29.5697 51.183 30.1761 51.4706 30.8219 51.5031C31.4676 51.5355 32.0998 51.3101 32.5794 50.8764L45.5725 39.1264L45.1759 47.0215C45.1598 47.3413 45.2069 47.6612 45.3144 47.9628C45.4220 48.2644 45.5878 48.5418 45.8026 48.7793C46.0174 49.0168 46.2768 49.2097 46.5661 49.3469C46.8554 49.4841 47.1689 49.5630 47.4887 49.5791C47.8085 49.5952 48.1283 49.5481 48.4299 49.4405C48.7315 49.3330 49.0090 49.1671 49.2465 48.9523C49.4840 48.7376 49.6768 48.4781 49.8141 48.1888C49.9513 47.8995 50.0302 47.5860 50.0463 47.2662L50.7383 33.4925Z"
+                                    fill="white"
+                                  />
+                                </svg>
+                                <h3 className="text-sm sm:text-base text-left leading-[20px] text-white font-medium">
+                                  {item.label}
+                                </h3>
+                              </div>
                             </div>
-                          </div>
-                        </>
-                      ) : (
-                        <p className="absolute bottom-3 sm:bottom-4 left-3 sm:left-4 text-white font-medium text-xs sm:text-sm">
-                          {item.label}
-                        </p>
-                      )}
+                          </>
+                        ) : (
+                          <p className="absolute bottom-3 sm:bottom-4 left-3 sm:left-4 text-white font-medium text-xs sm:text-sm">
+                            {item.label}
+                          </p>
+                        )}
+                      </div>
+                      <p className="mt-4 text-center font-medium text-base text-[#002453] whitespace-nowrap">
+                        {item.label}
+                      </p>
                     </div>
-                    <p className="mt-4 text-center font-medium text-base text-[#002453] whitespace-nowrap">
-                      {item.label}
-                    </p>
-                  </div>
+                  </Link>
                 ))}
               </Slider>
             </AnimatedSection>
